4b9c0f9ba8bf234eb5b567f03b13324258f9d327
Michael Villacrés <michael2003kiki@gmail.com>
2025-12-06 22:01:13 -0500

    Backend para produccion

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://proyecto-lectura-detector-estres.vercel.app",
 ]
 
 # CAMBIO IMPORTANTE: Permitir todo (*) para evitar errores en el primer deploy
