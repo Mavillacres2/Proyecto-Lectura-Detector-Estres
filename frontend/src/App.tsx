30de9d79cd1b87d72dbc6ad348232972d0890495
Michael Villacrés <michael2003kiki@gmail.com>
2025-12-15 00:15:21 -0500

    Frontend con Modelo v7

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,27 +4,24 @@ import Login from "././pages/auth/Login";
 import Register from "././pages/auth/Register";
 import { EmotionDetector } from "./components/EmotionDetector";
 import ResultsPage from "./pages/ResultsPage";
-import { ProtectedRoute } from "./routes/ProtectedRoute";
-import { PublicRoute } from "./routes/PublicRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         {/* Página inicial */}
         <Route path="/" element={<Home />} />
 
-        {/* Rutas públicas: si ya estás logueado, te manda a /detector */}
-        <Route element={<PublicRoute />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
+        {/* Login y Register */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-        {/* Rutas protegidas: requiere user_id (o token) */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/detector" element={<EmotionDetector />} />
-          <Route path="/results" element={<ResultsPage />} />
-        </Route>
+        {/* Detector SOLO después de login */}
+        <Route path="/detector" element={<EmotionDetector />} />
+
+        <Route path="/results" element={<ResultsPage />} />
+
       </Routes>
     </BrowserRouter>
   );
